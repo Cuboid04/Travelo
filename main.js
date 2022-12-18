c89938132678370544526b699ea2a53e372d751f
Fezz_Y <107613502+ItzJustDarkz99@users.noreply.github.com>
2022-12-18 20:31:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function train(){
+    window.location = "train.html";
+}
+function taxi(){
+    window.location = "taxi.html";
+}
+function bus(){
+    window.location = "bus.html";
+}
+function back(){
+    window.location = "index.html";
+}
+function start(){
+    window.location = "index.html";
+}
+function index(){
+    window.location = "start.html";
+}
